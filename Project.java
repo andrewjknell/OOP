e5f769eee2deb3de739fd82acaa4411db310afc3
andrewjknell <andrewjknell@yahoo.com>
2019-10-31 14:02:23 -0700

    halloweens

---
+++ Project.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Project {
+    public String name;
+    public String description;
+
+    public Project(){
+        
+    }
+    public Project(Sting n){
+        name = n;
+    }
+    public Project(String n, String desc){
+        name = n;
+        description = desc;
+    }
+    
+}
---
+++ ProjectTest.java
@@ -0,0 +1,8 @@
+public class ProjectTest{
+    public static void main(String[] args){
+        Project first = new Project();
+        Project second = new Project("Andrew");
+        Project third = new Project("andy", "cool project idea");
+        
+    }
+}
